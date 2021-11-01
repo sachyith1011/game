7a92798d4b144dc7a90803abec3e3975bccbcc58
sachyith1011 <74240638+sachyith1011@users.noreply.github.com>
2021-11-01 21:21:58 +0400

    Update game.js

--- game.js
+++ game.js
@@ -76,3 +76,74 @@ this.move = function(xDir, yDir) {
  this.y += this.dBear * yDir;
  this.display();
  };
+
+class Bee {
+ constructor(beeNumber) {
+ //the HTML element corresponding to the IMG of the bee
+ this.htmlElement = createBeeImg(beeNumber);
+//iits HTML ID
+ this.id = this.htmlElement.id;
+ //the left position (x)
+ this.x = this.htmlElement.offsetLeft;
+ //the top position (y)
+ this.y = this.htmlElement.offsetTop;
+ this.move = function(dx, dy) {
+ //move the bees by dx, dy
+ this.x += dx;
+ this.y += dy;
+ this.display();
+ };
+ this.display = function() {
+ //adjust position of bee and display it
+ this.fitBounds();//add this to adjust to bounds
+ this.htmlElement.style.left = this.x + "px";
+ this.htmlElement.style.top = this.y + "px";
+ this.htmlElement.style.display = "block";
+ };
+ this.fitBounds = function() {
+ //check and make sure the bees stays in the board space
+ let parent = this.htmlElement.parentElement;
+ let iw = this.htmlElement.offsetWidth;
+ let ih = this.htmlElement.offsetHeight;
+ let l = parent.offsetLeft;
+ let t = parent.offsetTop;
+ let w = parent.offsetWidth;
+ let h = parent.offsetHeight;
+ if (this.x < 0)
+ this.x = 0;
+ if (this.x > w - iw)
+ this.x = w - iw;
+ if (this.y < 0)
+ this.y = 0;
+ if (this.y > h - ih)
+ this.y = h - ih;
+ };
+ }
+}
+
+function createBeeImg(wNum) {
+ //get dimension and position of board div
+ let boardDiv = document.getElementById("board");
+ let boardDivW = boardDiv.offsetWidth;
+ let boardDivH = boardDiv.offsetHeight;
+ let boardDivX = boardDiv.offsetLeft;
+ let boardDivY = boardDiv.offsetTop;
+ //create the IMG element
+ let img = document.createElement("img");
+ img.setAttribute("src", "images/bee.gif");
+ img.setAttribute("width", "100");
+ img.setAttribute("alt", "A bee!");
+ img.setAttribute("id", "bee" + wNum);
+ img.setAttribute("class", "bee"); //set class of html tag img
+ //add the IMG element to the DOM as a child of the board div
+ img.style.position = "absolute";
+ boardDiv.appendChild(img);
+ //set initial position 
+ let x = getRandomInt(boardDivW);
+ let y = getRandomInt(boardDivH);
+ img.style.left = (boardDivX + x) + "px";
+ img.style.top = (y) + "px";
+ //return the img object
+ return img;
+}
+
