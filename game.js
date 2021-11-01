698ecff4fbc7f93b3cee03c2e99f8d78807746dd
sachyith1011 <74240638+sachyith1011@users.noreply.github.com>
2021-11-01 21:09:37 +0400

    Update game.js

--- game.js
+++ game.js
@@ -26,3 +26,25 @@ function Bear() {
  this.htmlElement.style.display = "absolute";
  };
 }
+
+// Handle keyboad events 
+// to move the bear
+function moveBear(e) {
+ //codes of the four keys
+ const KEYUP = 38;
+ const KEYDOWN = 40;
+ const KEYLEFT = 37;
+ const KEYRIGHT = 39;
+ if (e.keyCode == KEYRIGHT) {
+ bear.move(1, 0)
+ } // right key
+ if (e.keyCode == KEYLEFT) {
+ bear.move(-1, 0)
+ } // left key
+ if (e.keyCode == KEYUP) {
+ bear.move(0, -1)
+ } // up key
+ if (e.keyCode == KEYDOWN) {
+ bear.move(0, 1)
+ } // down key
+}
