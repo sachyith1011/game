69756a912150276d238154951fd825de5faef60e
sachyith1011 <74240638+sachyith1011@users.noreply.github.com>
2021-11-01 21:04:44 +0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
+<script src="game.js"></script>
+<noscript>
+ Warning - JavaScript is Disabled.
+ For full functionality of this page, it is necessary to enable JavaScript.
+</noscript>
 
+function Bear() {
+ this.dBear = 100;
+ this.htmlElement = document.getElementById("bear");
+ this.id = this.htmlElement.id;
+ this.x = this.htmlElement.offsetLeft;
+ this.y = this.htmlElement.offsetTop;
+ this.move = function(xDir, yDir) {
+ this.x += this.dBear * xDir;
+ this.y += this.dBear * yDir;
+ this.display();
+ };
+ this.display = function() {
+ this.htmlElement.style.left = this.x + "px";
+ this.htmlElement.style.top = this.y + "px";
+ this.htmlElement.style.display = "absolute";
+ };
+}
